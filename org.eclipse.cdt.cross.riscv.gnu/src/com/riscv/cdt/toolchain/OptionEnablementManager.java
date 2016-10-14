1630fc2f3125a1ff69aeaa89c5f14dac5764ed8a
Richard Herveille <richard.herveille@roalogic.com>
2016-10-14 15:40:31 +0200

    Fixed bug #1. 'Other options' affects all tools other-options option

--- org.eclipse.cdt.cross.riscv.gnu/src/com/riscv/cdt/toolchain/OptionEnablementManager.java
+++ org.eclipse.cdt.cross.riscv.gnu/src/com/riscv/cdt/toolchain/OptionEnablementManager.java
@@ -89,20 +89,20 @@ public class OptionEnablementManager extends AbstractOptionEnablementManager {
             
             // Get suffix of option and make sure all with same suffix
             // are set to same value.
-            // For example "riscv.asm.options.arc5core" and
-            // "riscv.compiler.options.arc5core" must match.
+            // For example "riscv.asm.options.optiona" and
+            // "riscv.compiler.options.optiona" must match.
             // HACK: except for ".level". We don't want optimization level to be mistaken for
             // debug level!
-            Object v = mgr.getValue(optionId);
-            if (v instanceof String || v instanceof Boolean) {
-                String suffix = getSuffixOf(optionId);
-                //Make copy to avoid occasional ConcurrentModificationException
-                for (String id : new ArrayList<String>(getOptionIds())) {
-                    if (suffix.equals(getSuffixOf(id)) && !id.equals(optionId) && !suffix.equals("level")) {
-                        setOptionValue(id, v);
-                    }
-                }
-            }
+//            Object v = mgr.getValue(optionId);
+//            if (v instanceof String || v instanceof Boolean) {
+//                String suffix = getSuffixOf(optionId);
+//                //Make copy to avoid occasional ConcurrentModificationException
+//                for (String id : new ArrayList<String>(getOptionIds())) {
+//                    if (suffix.equals(getSuffixOf(id)) && !id.equals(optionId) && !suffix.equals("level")) {
+//                        setOptionValue(id, v);
+//                    }
+//                }
+//           }
         }
 
 
